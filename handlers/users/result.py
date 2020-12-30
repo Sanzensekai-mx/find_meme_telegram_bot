af6bacfc32c1d2bce826ef5682ba56cb4f62c8b9
Sanzensekai-mx <ivanymn@yandex.ru>
2020-12-30 15:09:37 +0400

    Обработка исключений для вывода мема пользователю

--- handlers/users/result.py
+++ handlers/users/result.py
@@ -1,5 +1,6 @@
 import os
 import json
+from aiogram.utils.exceptions import BadRequest
 from .ten_random_memes import process_random_memes
 from aiogram.dispatcher import FSMContext
 from aiogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
@@ -9,18 +10,35 @@ from loader import dp, bot
 
 
 async def open_choice_meme(current_call, meme_data, meme_id):
-    detailed_inline_kb = InlineKeyboardMarkup().add(
-        InlineKeyboardButton('Подробнее',
-                             url=meme_data[result_mem_search_by_page[global_page.value][meme_id]]
-                             ['meme_href']
-                             ))
-    await bot.send_photo(
-        chat_id=current_call.from_user.id,
-        photo=meme_data[result_mem_search_by_page[global_page.value][meme_id]]
-        ['pic_href'])
-    await current_call.message.answer(
-        meme_data[result_mem_search_by_page[global_page.value][meme_id]]
-        ['describe'], reply_markup=detailed_inline_kb)
+    try:
+        detailed_inline_kb = InlineKeyboardMarkup().add(
+            InlineKeyboardButton('Подробнее',
+                                 url=meme_data[result_mem_search_by_page[global_page.value][meme_id]]
+                                 ['meme_href']
+                                 ))
+    except BadRequest:
+        await current_call.message.answer('Страница мема в интернете не найдена.')
+        # detailed_inline_kb = InlineKeyboardMarkup().add(
+        #     InlineKeyboardButton('Страница мема не найдена'))
+    try:
+        await bot.send_photo(
+            chat_id=current_call.from_user.id,
+            photo=meme_data[result_mem_search_by_page[global_page.value][meme_id]]
+            ['pic_href'])
+    except BadRequest:
+        await current_call.message.answer('Картинка не найдена.')
+    try:
+        await current_call.message.answer(
+            meme_data[result_mem_search_by_page[global_page.value][meme_id]]
+            ['describe'])
+    except BadRequest:
+        await current_call.message.answer('Описание мема не найдено.')
+    try:
+        # Добавить вывод названия сайта по записи в json?
+        await current_call.message.answer('Нажми кнопку, чтобы открыть странцу мема в источнике на memepedia.ru', reply_markup=detailed_inline_kb)
+    except Exception as e:
+        print(e)
+        await current_call.message.answer('Мем не имеет страницы в интернете')
 
 
 async def action_process_callback(call):
