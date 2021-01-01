ab539ac6426259a425393f59b4c457cb16bcb543
Sanzensekai-mx <ivanymn@yandex.ru>
2021-01-01 12:31:42 +0400

    Ветка для тестирования не на сервере

--- data/config.py
+++ data/config.py
@@ -3,17 +3,17 @@ from boto.s3.connection import S3Connection
 # from dotenv import load_dotenv
 
 # load_dotenv()
-BOT_TOKEN = S3Connection(os.environ['BOT_TOKEN'])
-# BOT_TOKEN = None
-admins = S3Connection(os.environ['admins'])
-# admins = []
+# BOT_TOKEN = S3Connection(os.environ['BOT_TOKEN'])
+BOT_TOKEN = None
+# admins = S3Connection(os.environ['admins'])
+admins = []
 
-# with open(os.path.join(os.getcwd(), 'data', 'secret_data.txt'), 'r') as f:
-#     secret_data = []
-#     for line in f:
-#         secret_data.append(line.strip())
-#     BOT_TOKEN = secret_data[0]
-#     for elem in secret_data[1:]:
-#         admins.append(elem)
+with open(os.path.join(os.getcwd(), 'data', 'secret_data.txt'), 'r') as f:
+    secret_data = []
+    for line in f:
+        secret_data.append(line.strip())
+    BOT_TOKEN = secret_data[0]
+    for elem in secret_data[1:]:
+        admins.append(elem)
 
 # ip = os.getenv("ip")
