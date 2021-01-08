bc63335c7c56e55904fe70cb7cf30710b59d8fb5
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2021-01-08 15:56:34 +0400

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,19 +1,5 @@
 import os
-# from boto.s3.connection import S3Connection
-# from dotenv import load_dotenv
 
-# load_dotenv()
 BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))
-# BOT_TOKEN = None
-admins = str(os.environ.get('admins')).split(', ')
-# admins = []
-#
-# with open(os.path.join(os.getcwd(), 'data', 'secret_data.txt'), 'r') as f:
-#     secret_data = []
-#     for line in f:
-#         secret_data.append(line.strip())
-#     BOT_TOKEN = secret_data[0]
-#     for elem in secret_data[1:]:
-#         admins.append(elem)
 
-# ip = os.getenv("ip")
+admins = str(os.environ.get('admins')).split(', ')
