c4820dc29790dc699befeab1e8cc6751ca8573cc
Sanzensekai-mx <ivanymn@yandex.ru>
2021-01-31 23:37:09 +0400

    update config

--- data/config.py
+++ data/config.py
@@ -3,14 +3,15 @@ import os
 # load_dotenv()
 BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))
 admins = str(os.environ.get('admins')).split(', ')
+POSTGRES_URI = str(os.environ.get('DATABASE_URL'))
 
-host = str(os.environ.get("PG_HOST"))  # хост базы данных
-PG_USER = str(os.environ.get("PG_USER"))  # имя владельца базы данных
-PG_PASS = str(os.environ.get("PG_PASS"))  # пароль бд
-DATABASE = str(os.environ.get("DATABASE"))  # имя БД в pgAdmin
-
-# ip = str(os.environ.get("ip"))
-
-# Ссылка подключения к базе данных
-POSTGRES_URI = f"postgresql://{PG_USER}:{PG_PASS}@{host}/{DATABASE}"
+# host = str(os.environ.get("PG_HOST"))  # хост базы данных
+# PG_USER = str(os.environ.get("PG_USER"))  # имя владельца базы данных
+# PG_PASS = str(os.environ.get("PG_PASS"))  # пароль бд
+# DATABASE = str(os.environ.get("DATABASE"))  # имя БД в pgAdmin
+#
+# # ip = str(os.environ.get("ip"))
+#
+# # Ссылка подключения к базе данных
+# POSTGRES_URI = f"postgresql://{PG_USER}:{PG_PASS}@{host}/{DATABASE}"
 
