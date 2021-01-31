6c050363bc57996bb4675360ada9bd9b6d2a3a21
Sanzensekai-mx <ivanymn@yandex.ru>
2021-01-31 23:25:54 +0400

    Удаление излишков

--- parse/old_meme_create.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('old_mem_dataset.json', 'w', encoding='utf-8') as old_meme, \
-        open('mem_dataset.json', 'r', encoding='utf-8') as now_old_meme:
-    current_old_memes = json.load(now_old_meme)
-    json.dump(current_old_memes, old_meme, indent=4, ensure_ascii=False)
